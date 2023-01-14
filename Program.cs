3cab98607d7b66e345dd540efc97e89bf34b9d0b
Nata Konstanta <rio201150@gmail.com>
2023-01-14 18:53:31 +0500

    10 задача

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.WriteLine("Введите число от 1 до 7");
+// int x = Convert.ToInt32 (Console.ReadLine());
+// if (x == 1) Console.WriteLine ("Рабочий день!");
+// else if (x == 2) Console.WriteLine ("Рабочий день!");
+// else if (x == 3) Console.WriteLine ("Рабочий день!");
+// else if (x == 4) Console.WriteLine ("Рабочий день!");
+// else if (x == 5) Console.WriteLine ("Рабочий день!");
+// else if (x == 6) Console.WriteLine ("Выходной день!");
+// else if (x == 7) Console.WriteLine ("Выходной день!");
+// else Console.WriteLine("Введенный день не корректный");
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Обращаться к числу как к строке нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите трёхзначное целое число");
+int x = Convert.ToInt32(Console.ReadLine());
+int n = x;
+if (x <= 9)
+{
+    Console.WriteLine("Второй цифры нет");
+}
+else
+{
+    while (x>99)
+    {
+        x = x / 10;
+    }
+    int result = x % 10;
+    Console.WriteLine($"{result} является второй цифрой от числа {n}");
+ }
