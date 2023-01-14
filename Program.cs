36ad3cac71acb96a7c46c8bb3529ca6f27582a9e
Nata Konstanta <rio201150@gmail.com>
2023-01-14 18:59:04 +0500

    13 задача

--- Program.cs
+++ Program.cs
@@ -22,19 +22,41 @@
 // 918 -> 1
 
 
-Console.WriteLine("Введите трёхзначное целое число");
+// Console.WriteLine("Введите трёхзначное целое число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int n = x;
+// if (x <= 9)
+// {
+//     Console.WriteLine("Второй цифры нет");
+// }
+// else
+// {
+//     while (x>99)
+//     {
+//         x = x / 10;
+//     }
+//     int result = x % 10;
+//     Console.WriteLine($"{result} является второй цифрой от числа {n}");
+//  }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Обращаться к числу как к строке нельзя.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите целое число");
 int x = Convert.ToInt32(Console.ReadLine());
 int n = x;
-if (x <= 9)
+if (x <= 99)
 {
-    Console.WriteLine("Второй цифры нет");
+    Console.WriteLine("третьей цифры нет");
 }
 else
 {
-    while (x>99)
+    while (x>999)
     {
         x = x / 10;
     }
     int result = x % 10;
-    Console.WriteLine($"{result} является второй цифрой от числа {n}");
- }
+    Console.WriteLine($"{result} является третьей цифрой от числа {n}");
+}
